02235e0c9de43f1bea48be2d65b2c9768081439f
Jeffrey Yoo <jeffyoo216@gmail.com>
2018-09-24 21:15:56 -0500

    Added a getTime function that console logs the time. Abstracted a portion of the code into a new function, findKeyword, that finds the keyword in the post. Still need to work on establishing an object to check against new posts.

--- subreddit-parser.js
+++ subreddit-parser.js
@@ -1,15 +1,73 @@
 const https = require('https');
 
-https.get('https://www.reddit.com/r/hardwareswap/new.json?sort=new', (res) => {
-    console.log('res: ', res.data);
+const url = 'https://www.reddit.com/r/hardwareswap/new.json?sort=new';
 
-    // console.log(res.data.children);
+function getPosts(url, keyword) {
+	getTime();
 
-    // res.data.children.forEach((post, index) => {
-    //     console.log('#' + index);
-    //     console.log('Title: ', post.title);
-    //     console.log('Body: ', post.selftext);
-    // })
+	https.get(url, (res) => {
+		let data = '';
+
+		res.on('data', (chunk) => {
+			data += chunk;
+		});
+
+		res.on('end', () => {
+			const posts = JSON.parse(data).data.children;
+			findKeyword(posts, keyword);
+		})
+	});
+};
+
+function findKeyword(posts, keyword) {
+	console.log('length: ', posts.length);
+
+	posts.forEach(postObj => {
+		const p = postObj.data;
+		if (p.selftext.includes(keyword) || p.title.includes(keyword)) {
+			// Check if it's in the 'DB'
+				// If it is not, save it then show it
+				// If it is, then skip
 
 
-});
+			console.log(p.title);
+			console.log(p.selftext);
+		}
+	})
+}
+
+function getTime() {
+	const d = new Date();
+	const month = d.getMonth() + 1;
+	const day = d.getDate();
+	const year = d.getFullYear();
+	const hour = getHour(d.getHours()) 
+  const minutes = d.getMinutes();
+	const seconds = d.getSeconds();
+	const amOrPm = getAmOrPm(d.getHours());
+
+	console.log('Running getPosts on ' + month + '/' + day + '/' + year + ' at ' + hour + ':' + minutes + ':' + seconds + amOrPm);
+}
+
+function getHour(hour) {
+	if (hour > 12) {
+		return hour - 12;
+	} else {
+		return hour;
+	}
+}
+
+function getAmOrPm(hour) {
+	if (hour >= 12) {
+		return 'pm';
+	} else {
+		return 'am';
+	}
+}
+
+setInterval(() => {
+	getPosts(url, '1080');
+}, 1000);
+// }, 300000)
+
+// getPosts(url);
